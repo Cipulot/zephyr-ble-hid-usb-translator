ee3f3f9975f0b44fb925427743a2a4c438282daa
Cipulot <40441626+Cipulot@users.noreply.github.com>
2024-05-25 18:28:08 +0200

    added script

---
+++ env.sh
@@ -0,0 +1,6 @@
+export PATH=/opt/nordic/ncs/toolchains/580e4ef81c/bin:/opt/nordic/ncs/toolchains/580e4ef81c/usr/bin:/opt/nordic/ncs/toolchains/580e4ef81c/usr/local/bin:/opt/nordic/ncs/toolchains/580e4ef81c/opt/bin:/opt/nordic/ncs/toolchains/580e4ef81c/opt/nanopb/generator-bin:/opt/nordic/ncs/toolchains/580e4ef81c/opt/zephyr-sdk/aarch64-zephyr-elf/bin:/opt/nordic/ncs/toolchains/580e4ef81c/opt/zephyr-sdk/x86_64-zephyr-elf/bin:/opt/nordic/ncs/toolchains/580e4ef81c/opt/zephyr-sdk/arm-zephyr-eabi/bin:/opt/nordic/ncs/toolchains/580e4ef81c/opt/zephyr-sdk/riscv64-zephyr-elf/bin:$PATH
+export GIT_EXEC_PATH=/opt/nordic/ncs/toolchains/580e4ef81c/Cellar/git/2.37.3/libexec/git-core
+export GIT_TEMPLATE_DIR=/opt/nordic/ncs/toolchains/580e4ef81c/Cellar/git/2.37.3/share/git-core/templates
+export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
+export ZEPHYR_SDK_INSTALL_DIR=/opt/nordic/ncs/toolchains/580e4ef81c/opt/zephyr-sdk
+export ZEPHYR_BASE=/opt/nordic/ncs/v2.6.1/zephyr
